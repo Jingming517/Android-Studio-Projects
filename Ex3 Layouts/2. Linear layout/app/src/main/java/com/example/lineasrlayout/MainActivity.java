ae973c6f07dacc27f6cc294ac3ae606281dab1b7
Jingming517 <53804726+Jingming517@users.noreply.github.com>
2021-07-30 10:55:39 +0800

    Ex3 Layouts

---
+++ Ex3 Layouts/2. Linear layout/app/src/main/java/com/example/lineasrlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lineasrlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
